37ede8e74ebf0682f3500c21cd91139e46fb2f4e
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-02-26 14:07:08 +0100

    Updated docs regarding timeout settings in tests

--- test/index.test.js
+++ test/index.test.js
@@ -7,9 +7,9 @@ chai.should()
 // Write your tests here
 describe('template-action', function () {
   // this.timeout(10000)
-  
+
   // uncomment this if you want to raise the timeout cap for your whole test (when you need to perform long async tasks). You can also follow the same logic per describe block or per it block to increase the timeout cap
-  // if your describe/it callbacks are arrow functions, you need to use the following syntax: describe('...', () => {}).timeout(4000)
+  // if your describe/it callbacks are arrow functions, you need to use the following syntax: describe('...', () => {}).timeout(...) but be aware that this won't apply to hooks!
 })
 // *************
 
