ea786bbaa2951fe47c7a7e5c19c8d41abcccad08
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-08-20 13:56:23 +0200

    Removed init for .gitignore node_modules removal

--- .github/workflows/scripts/init.sh
+++ .github/workflows/scripts/init.sh
@@ -4,8 +4,6 @@ npm i kaskadi-cli
 # Init
 ./node_modules/.bin/kaskadi init action
 npm rm kaskadi-cli
-# Delete last line of .gitignore to stop ignoring node_modules
-dd if=/dev/null of=.gitignore bs=1 seek=$(echo $(stat --format=%s .gitignore ) - $( tail -n2 .gitignore | wc -c) | bc )
 # Commit files
 git rm .github/workflows/init.yml
 git rm .github/workflows/scripts/init.sh
