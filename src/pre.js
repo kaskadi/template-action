29ac03d3e61862f23069f1863a36479d53e28fc3
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-08-24 19:30:03 +0200

    Simplified pre step logic

--- src/pre.js
+++ src/pre.js
@@ -1,12 +1,24 @@
 const { spawnSync } = require('child_process')
-if (process.env.GITHUB_ACTIONS && process.env.GITHUB_REPOSITORY !== 'kaskadi/template-action') {
+const path = require('path')
+
+function main () {
+  const actionRepo = path.join(__dirname, '..')
   const callingRepo = process.cwd()
-  process.chdir(__dirname)
-  process.chdir('..')
+  if (callingRepo !== actionRepo) {
+    process.chdir(actionRepo)
+    installDependencies()
+    process.chdir(callingRepo)
+  } else {
+    installDependencies()
+  }
+}
+
+function installDependencies () {
   console.log('INFO: installing action dependencies...')
   console.log('************ NPM ouput ************')
   spawnSync('npm', ['i', '--only=prod'], { stdio: 'inherit' })
   console.log('************ End of NPM ouput ************')
   console.log('SUCCESS: dependencies installed!')
-  process.chdir(callingRepo)
 }
+
+main()
