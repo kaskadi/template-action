7c0ea6980717761ad06099b0c79557f24157426e
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-08-24 11:50:46 +0200

    Refactored action execution function in tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,5 @@
 /* eslint-env mocha */
-const childProc = require('child_process')
+const { spawnSync } = require('child_process')
 const chai = require('chai')
 chai.should()
 
@@ -7,25 +7,26 @@ chai.should()
 // Write your tests here
 describe('template-action', function () {
   // this.timeout(10000)
-
   // uncomment this if you want to raise the timeout cap for your whole test (when you need to perform long async tasks). You can also follow the same logic per describe block or per it block to increase the timeout cap
   // if your describe/it callbacks are arrow functions, you need to use the following syntax: describe('...', () => {}).timeout(...) but be aware that this won't apply to hooks!
+  before(function () {
+    runAction()
+  })
 })
 // *************
 
-// awaitable function to execute the main script of this action
-function execMain () {
-  return new Promise((resolve, reject) => {
-    childProc.exec('node index', (err, stdout, stderr) => {
-      if (err === null) {
-        console.log(stdout)
-        resolve(true)
-      } else {
-        console.log(stderr)
-        resolve(false)
-      }
-    })
-  })
+// function to execute the main script of this action
+function runAction () {
+  try {
+    console.log('INFO: running action...')
+    spawnSync('node', ['src/pre'], { stdio: 'inherit' })
+    spawnSync('node', ['src/main'], { stdio: 'inherit' })
+    spawnSync('node', ['src/post'], { stdio: 'inherit' })
+  } catch (err) {
+    console.log('ERROR: an error occured...')
+    console.log(err)
+    process.exit()
+  }
 }
 
 // just add any helpers you may need
