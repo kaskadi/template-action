210c9b724f39b6b37dc4cd572cb661f55979f674
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-08-24 17:26:52 +0200

    Improved run-action resilience

--- test/helpers/run-action.js
+++ test/helpers/run-action.js
@@ -1,4 +1,5 @@
 const { spawn } = require('child_process')
+const path = require('path')
 
 module.exports = async (steps) => {
   for (const step of steps) {
@@ -8,7 +9,7 @@ module.exports = async (steps) => {
 
 function runStep (step) {
   console.log(`************ running action ${step} step ************\n`)
-  const proc = spawn('node', [`src/${step}`])
+  const proc = spawn('node', [path.join(__dirname, '../..', `src/${step}`)])
   return new Promise((resolve, reject) => {
     proc.stdout.on('data', data => {
       console.log(data.toString().trim())
